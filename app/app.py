50e56871c55e0c7bab19d95fe861d16866684a65
--replace <j.canfora@icloud.com>
2025-01-31 17:23:13 +0100

    basic query to database from html works

--- app/app.py
+++ app/app.py
@@ -3,10 +3,10 @@ Main entry point of the application.
 """
 
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_migrate import Migrate
 from app.resources import api
-from app.models import db
+from app.models import db, Vendor
 
 def test_something():
     print('this is a test')
@@ -17,10 +17,6 @@ def create_app():
     Application factory function to create and configure the Flask app.
     """
     app = Flask(__name__)
-
-    # # Set up configurations for the app
-    # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///instance/vendors.db"
-    # app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     # Set the absolute path for the SQLite database file
     basedir = os.path.abspath(os.path.dirname(__file__))
@@ -37,7 +33,7 @@ def create_app():
     @app.route('/')
     def home():
         return render_template('index.html')  # Render the HTML template
-
+    
     @app.route("/contact")
     def contact():
         return render_template("contact.html")
@@ -50,11 +46,29 @@ def create_app():
     def privacy():
         return render_template("privacy.html")
 
-    return app
+    @app.route('/search', methods=['GET', 'POST'])
+    def search():
+        if request.method == 'POST':
+            country = request.form.get('country')
+            service = request.form.get('service')
+            search_term = request.form.get('search')
 
+            # Query the database based on the form data
+            query = Vendor.query
+            if country:
+                query = query.filter(Vendor.address.contains(country))
+            if service:
+                query = query.filter(Vendor.service_type == service)
+            if search_term:
+                query = query.filter(Vendor.name.contains(search_term))
+
+            results = query.all()
+
+            return render_template('results.html', results=results)
+        return render_template('index.html')
+
+    return app
 
 if __name__ == "__main__":
     app = create_app()
-    with app.app_context():
-        db.create_all()  # Create tables directly
     app.run(debug=True)
